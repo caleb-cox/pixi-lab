3faa30142fc27cde6c212e74a3f78db3325ceed6
calebcox725 <caleb.cox@flatironschool.com>
2017-06-30 11:53:54 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'sanitize.css';
+import * as PIXI from 'pixi.js';
+
+const renderer = PIXI.autoDetectRenderer(256, 256);
+
+document.body.appendChild(renderer.view);
+
+const stage = new PIXI.Container();
+
+renderer.render(stage);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  devtool: 'cheap-module-eval-source-map',
+  plugins: [
+    HtmlWebpackPluginConfig,
+  ],
+};
